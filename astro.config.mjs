967a7c23126cd6f4a235975eb45c28e52a0add84
Nik <nikhilshivpuriya29@gmail.com>
2025-10-13 00:37:41 +0530

    Birthday surprise for Nupur

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+
+export default defineConfig({
+  integrations: [tailwind()],
+  output: "static",
+  compressHTML: true,
+  build: {
+    inlineStylesheets: "auto",
+  },
+});
