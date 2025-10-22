ca719d4882223af82f7a87183b069d0ea25bae39
Nik <nikhilshivpuriya29@gmail.com>
2025-10-22 12:35:57 +0530

    fix: Configure Cloudflare adapter for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import cloudflare from '@astrojs/cloudflare';
+
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',  // ADD THIS LINE
+
+  // ADD THIS LINE
+  output: 'server',
+
   compressHTML: true,
+
   build: {
     inlineStylesheets: 'auto'
-  }
+  },
+
+  adapter: cloudflare()
 });
