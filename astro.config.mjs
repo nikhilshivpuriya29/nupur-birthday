6812b8bb6064b967c2ceb3d66689f0d332d7c37d
Nik <nikhilshivpuriya29@gmail.com>
2025-10-24 00:24:49 +0530

    fix: Final working dependency configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   integrations: [tailwind()],
-
-  // ADD THIS LINE
   output: 'server',
-
-  compressHTML: true,
-
-  build: {
-    inlineStylesheets: 'auto'
-  },
-
   adapter: cloudflare()
 });
